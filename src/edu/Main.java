2feabc2533acfbbab713bf4757aeaff2ffced9f1
서동후 <@192.168.219.100>
2021-10-07 12:56:48 +0900

    첫프로젝트

---
+++ src/edu/Main.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package edu;
+
+/**
+ * @author ¼­µ¿ÈÄ
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Reporsitory");
+
+	}
+
+}
